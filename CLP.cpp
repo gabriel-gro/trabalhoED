5b871560f2e41fd5c7aa6d343f6aa1c3c875f83a
gabriel-gro <gabriel.ribolive@gmail.com>
2016-11-17 00:29:18 -0200

    Add files via upload
    funcional- INSERIR, EXCLUIR, LISTAR
    atribuicoes - funcao posInsercao(), e menu()
    insercao agora é feita em cima de um registro NAO ativo

--- CLP.cpp
+++ CLP.cpp
@@ -12,16 +12,16 @@ typedef struct{
     int versao;
     char autor[20];
     char tipo[12];
-
+    bool ativo;
 }LingProg;
 
 int menu();
 void inserir();
-//void remove();
+void remove();
 void listar();
 //LingProg buscar();
-//CLP* transfere( CLP clp02 , opcao);
-//int posInsercao();
+//CLP* transfere(CLP clp02 , opcao);
+int posInsercao();
 
 
 int main()
@@ -34,6 +34,9 @@ int main()
                 break;
             case 1:
                 inserir();
+                break;
+            case 2:
+                remove();
                 break;
             case 3:
                 listar();
@@ -66,10 +69,9 @@ int menu(){
 }   
 
 void inserir(){
-    LingProg novo;                  // criando uma variavel do tipo LingProg para atribuir no arquivo
-    
+    LingProg novo;                                              // criando uma variavel do tipo LingProg para atribuir no arquivo
     // Abrindo arquivo em modo de leitura
-    ofstream arquivo("dados.dat", ios::binary | ios::app);      //Abrindo arquivo ( modo binario, falando que so se pode acrecentar ) ios::binary|ios::append
+    fstream arquivo("dados.dat", ios::binary | ios::in | ios::out );      //Abrindo arquivo ( modo binario, falando que so se pode acrecentar ) ios::binary|ios::append
         
     
     if(!arquivo){                   // Testa se arquivo contem erro    
@@ -97,6 +99,9 @@ void inserir(){
     cout << "|Tipo: ";              // Tipo;  exemplos: Funcional, lógica, imperativa, POO, etc...
     cin.getline(novo.tipo, 12);     // perde o 1º caracter da linha ao inserir
     
+    novo.ativo = true;
+    
+    arquivo.seekp(posInsercao());
     arquivo.write((const char *)&novo, sizeof(LingProg));    // Escrita em arquivo binario, mandando a referencia do obj e o tamanho
     cout << "|-------------------------------------------------------|" << endl;
     
@@ -109,32 +114,93 @@ void listar(){
     LingProg dados;
     
     while(arquivo.read((char *)&dados, sizeof(LingProg))){
-        cout << "|Nome: " << dados.nome << endl
-             << "|Ano De Criacao: " << dados.anoCriacao << endl
-             << "|Ultimo Acesso: " << dados.ultimoAcesso << endl
-             << "|Versao: " << dados.versao << endl
-             << "|Autor: " << dados.autor << endl
-             << "|Tipo: " << dados.tipo << endl
-             << endl;   
+        //if(dados.ativo){
+            cout << "|Nome: " << dados.nome << endl
+                 << "|Ano De Criacao: " << dados.anoCriacao << endl
+                 << "|Ultimo Acesso: " << dados.ultimoAcesso << endl
+                 << "|Versao: " << dados.versao << endl
+                 << "|Autor: " << dados.autor << endl
+                 << "|Tipo: " << dados.tipo << endl
+                 << "|Ativo: "<< dados.ativo << endl
+                 << endl; 
+        //}
     }
 }
 
-//int posInsercao(){
-        
-    //ifstream arquivo("dados.dat",ios::binary);      // Abre o arquivo no modo leitura
-    //arquivo.seekg(0);                               // posicao de leitura no inicio arquivo
+void remove(){
+    LingProg dado;
+    fstream arquivo("dados.dat", ios::binary | ios::in | ios::out);
+    arquivo.seekg(0);
+    arquivo.seekp(0);
     
-    //LingProg elementos;                             // Variavel do tipo criado ( LingProg)
-    //int pos=0;                                      // Variavel verificador de posicao
+    char nome[20];
+    cout << "Digite o nome da Linguagem a ser removida" <<endl;
+    cin.ignore();
+    cin.getline(nome, 20);
     
-    //while(arquivo.read((char *)&elementos, sizeof(LingProg)){
-        //if(strcmp(elementos.nome, "") == 0){        
-            //break;
+    int pos = 0;
+    while(arquivo.read((char *)&dado, sizeof(LingProg))){
+        if(strcmp(dado.nome, nome) == 0)
+            break;
+        ++pos;
+    }
+    if((strcmp(dado.nome, nome) == 0) and (dado.ativo)){
+        dado.ativo = false;
+        arquivo.seekp(pos * sizeof(LingProg));
+        arquivo.write((const char *)&dado, sizeof(LingProg));
+    }
+    else
+        cout << "Linguagem nao foi encontrada" << endl;
+    arquivo.close();
+}
+
+//void buscar(){
+    //LingProg dado;
+    
+    //ifstream arquivo("dados.dat", ios::binary);
+    //arquivo.seekg(0);
+    //char nome[20];
+    //cout << "Digite o nome da Linguagem a ser removida" <<endl;
+    //cin.ignore();
+    //cin.getline(nome, 20);
+    
+    //int pos = 0;
+    //while(arquivo.read((char *)&dado, sizeof(LingProg))){
+        //if(strcmp(dado.nome, nome) == 0){
+            //cout << "|Nome: " << dado.nome << endl
+                 //<< "|Ano De Criacao: " << dado.anoCriacao << endl
+                 //<< "|Ultimo Acesso: " << dado.ultimoAcesso << endl
+                 //<< "|Versao: " << dado.versao << endl
+                 //<< "|Autor: " << dado.autor << endl
+                 //<< "|Tipo: " << dado.tipo << endl
+                 //<< endl; 
         //}
-        
         //++pos;
     //}
+    
 //}
+
+
+
+// quando implementar se registro ta ativo ou nao
+
+int posInsercao(){
+        
+    ifstream arquivo("dados.dat",ios::binary);      // Abre o arquivo no modo leitura
+    arquivo.seekg(0);                               // posicao de leitura no inicio arquivo
+    
+    LingProg elementos;                             // Variavel do tipo criado ( LingProg)
+    int pos=0;                                      // Variavel verificador de posicao
+    
+    while(arquivo.read((char *)&elementos, sizeof(LingProg))){
+        if(!elementos.ativo){                        //  quando encontra um elemento excluido anteriormente
+            break;
+        }
+    ++pos;
+    }
+    arquivo.close();
+    return pos * int(sizeof(LingProg));
+}
 
 
 
